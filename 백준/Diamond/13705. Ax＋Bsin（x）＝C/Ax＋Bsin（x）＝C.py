c24bcd9d1fc6dd1a296ffd377dfb3941c3dd853d
김륜교 <165171093+kimryungyo@users.noreply.github.com>
2024-12-03 19:12:41 +0900

    Ax＋Bsin（x）＝C.py 업데이트

--- 백준/Diamond/13705. Ax＋Bsin（x）＝C/Ax＋Bsin（x）＝C.py
+++ 백준/Diamond/13705. Ax＋Bsin（x）＝C/Ax＋Bsin（x）＝C.py
@@ -1,40 +1,48 @@
 from decimal import Decimal, getcontext
 
+# 각 값의 정확도 설정 (소수점 이하 x자리)
 PI_PREC = 128
-SIN_PREC = 64
-BIN_PREC = 64
+SIN_PREC = 128
+BIN_PREC = 128
 
-ALL_PREC = max(SIN_PREC, BIN_PREC) + 16
+# 정확도 적용
+ALL_PREC = max(PI_PREC, SIN_PREC, BIN_PREC) + 16
 getcontext().prec = ALL_PREC
 
-PI = Decimal("3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460")
+# 파이값 128자리
+PI = Decimal("3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460"[:PI_PREC])
 
-def sin(x):
+
+
+# 테일러 급수를 이용해 sin값을 소수점 이하 약 128자리까지 정확하게 구하는 함수
+# x값이 커지면 Decimal에서도 오차가 발생할 수 있기 때문에 주기성을 활용해야 한다
+def sin(pi: Decimal):
     epsilon = Decimal(f'1e-{SIN_PREC}')
-    TWO_PI = 2 * PI
+    pi %= PI * Decimal("2")
 
-    x = x % TWO_PI
+    sin_ = Decimal("0")
+    nume = deno = Decimal("1")
+    mulpi = pi
+    for n in range(512):
+        add_ = nume * mulpi / deno
+        sin_ += add_
+        if abs(add_) < epsilon: break
+        
+        nume *= -1
+        deno *= (n * 2 + 2) * (n * 2 + 3)
+        mulpi *= pi * pi
 
-    exp = deno = sign = 1
-    result = powers = Decimal(x)
-    x_suqare = Decimal(x) ** 2
+    return sin_
 
-    while True:
-        exp += 2
-        deno *= exp * (exp - 1)
-        powers *= x_suqare
-        sign *= -1
-        term = (powers / Decimal(deno)) * sign
-        result += term
 
-        if abs(term) < epsilon:
-            break
 
-    return result
-
+# Ax + Bsin(x) - C 값을 반환하는 함수
 def function(x):
     return A * x + B * sin(x) - C
 
+
+
+# 오차가 소수점 이하 128자리가 될 때 까지 이분 탐색을 하는 함수
 def binary_search():
     epsilon = Decimal(f'1e-{BIN_PREC}')
 
@@ -48,6 +56,8 @@ def binary_search():
 
     return left
 
+
+# Ax + Bsin(x) = C 을 만족하는 x값을 반올림해 소수점 여섯째까지 출력하는 코드 
 A, B, C = map(int, input().split())
 result = binary_search()
 print(f"{round(result, 6):.6f}")
