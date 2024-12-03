e7b116afa37ae69a4cb831ee0c87b56c72df60dd
김륜교 <165171093+kimryungyo@users.noreply.github.com>
2024-12-04 02:29:53 +0900

    Update 할 수 있다.py

--- 백준/Platinum/1287. 할 수 있다/할 수 있다.py
+++ 백준/Platinum/1287. 할 수 있다/할 수 있다.py
@@ -1,16 +1,15 @@
-from re import sub
+import re
 def q(r):
- b=c="";d=0 
+ b=c='';d=0
  for e in r:
-  if e=="(":d+=1
-  elif e==")":d-=1
+  if e=='(':d+=1
+  elif e==')':d-=1
   if d>100:c+=e
-  elif c:c+=")";b+=str(q(c));c=""
+  elif c:c+=')';b+=str(q(c));c=''
   else:b+=e
  return eval(b)
-def n(z):return str(int(z.group()))
-m=sub(r'\b0+(\d+)\b',n,input())
+m=re.sub(r'\b0+(\d+)\b',lambda z:str(int(z.group())),input())
 try:
- if "()" in m:raise 0
- l="".join("&" if h in {"+","-","*","/"} else h for h in m);q(l);print(q(m.replace("/","//")))
+ if'()'in m:raise
+ q(''.join('&'if h in'+-*/'else h for h in m));print(q(m.replace('/','//')))
 except:print("ROCK")
