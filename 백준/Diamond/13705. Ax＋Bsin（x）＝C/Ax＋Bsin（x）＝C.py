b2f0e684ddc5f20e3ee6bbefbfb0a8c38d3d62c2
김륜교 <165171093+kimryungyo@users.noreply.github.com>
2024-12-03 17:01:45 +0900

    [Diamond V] Title: Ax+Bsin(x)=C, Time: 856 ms, Memory: 143480 KB -BaekjoonHub

--- 백준/Diamond/13705. Ax＋Bsin（x）＝C/Ax＋Bsin（x）＝C.py
+++ 백준/Diamond/13705. Ax＋Bsin（x）＝C/Ax＋Bsin（x）＝C.py
@@ -1,24 +1,19 @@
 from decimal import Decimal, getcontext
 
-# 각 값의 정확도 설정 (소수점 이하 x자리)
 PI_PREC = 128
-SIN_PREC = 128
-BIN_PREC = 128
+SIN_PREC = 64
+BIN_PREC = 64
 
-# 정확도 적용
-ALL_PREC = max(PI_PREC, SIN_PREC, BIN_PREC) + 16
+ALL_PREC = max(SIN_PREC, BIN_PREC) + 16
 getcontext().prec = ALL_PREC
 
-# 파이값 128자리
 PI = Decimal("3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460")
 
-
-
-# 테일러 급수를 이용해 sin값을 소수점 이하 약 128자리까지 정확하게 구하는 함수
-# x값이 커지면 Decimal에서도 오차가 발생할 수 있기 때문에 주기성을 활용해야 한다
 def sin(x):
     epsilon = Decimal(f'1e-{SIN_PREC}')
-    x = x % (2 * PI)
+    TWO_PI = 2 * PI
+
+    x = x % TWO_PI
 
     exp = deno = sign = 1
     result = powers = Decimal(x)
@@ -37,15 +32,9 @@ def sin(x):
 
     return result
 
-
-
-# Ax + Bsin(x) - C 값을 반환하는 함수
 def function(x):
     return A * x + B * sin(x) - C
 
-
-
-# 오차가 소수점 이하 128자리가 될 때 까지 이분 탐색을 하는 함수
 def binary_search():
     epsilon = Decimal(f'1e-{BIN_PREC}')
 
@@ -59,8 +48,6 @@ def binary_search():
 
     return left
 
-
-# Ax + Bsin(x) = C 을 만족하는 x값을 반올림해 소수점 여섯째까지 출력하는 코드 
 A, B, C = map(int, input().split())
 result = binary_search()
 print(f"{round(result, 6):.6f}")
