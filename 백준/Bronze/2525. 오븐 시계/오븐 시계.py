8ef0ee971724bff0edbac20b78de8eed653d12b3
김륜교 <165171093+kimryungyo@users.noreply.github.com>
2024-12-07 03:15:24 +0900

    [Bronze III] Title: 오븐 시계, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -0,0 +1,11 @@
+A, B = map(int, input().split())
+C = int(input())
+
+total_minutes = A * 60 + B
+
+total_minutes += C
+
+end_hour = (total_minutes // 60) % 24
+end_minute = total_minutes % 60
+
+print(end_hour, end_minute)
