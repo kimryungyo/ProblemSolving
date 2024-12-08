0d045166aa178d28b21802fce9a941f6fc205bd7
김륜교 <165171093+kimryungyo@users.noreply.github.com>
2024-12-08 21:39:23 +0900

    [Gold III] Title: 불필요한 수, Time: 140 ms, Memory: 124872 KB -BaekjoonHub

--- 백준/Gold/2378. 불필요한 수/불필요한 수.py
+++ 백준/Gold/2378. 불필요한 수/불필요한 수.py
@@ -17,38 +17,41 @@ def prime_factorization(x):
         factors[num] = factors.get(num, 0) + 1
     return factors
 
-factors = prime_factorization(M)
-n = N - 1
+def main():
+    factors = prime_factorization(M)
+    n = N - 1
 
-factorial_exp = {}
-for p in factors.keys():
-    exponent_p_of_i = [0] * (n + 1)
+    factorial_exp = {}
+    for p in factors.keys():
+        exponent_p_of_i = [0] * (n + 1)
 
-    power = p
-    while power <= n:
-        for multiple in range(power, n + 1, power):
-            exponent_p_of_i[multiple] += 1
-        power *= p
+        power = p
+        while power <= n:
+            for multiple in range(power, n + 1, power):
+                exponent_p_of_i[multiple] += 1
+            power *= p
 
-    factorial_p_exp_p = [0] * (n + 1)
-    for i in range(1, n+1):
-        factorial_p_exp_p[i] = factorial_p_exp_p[i-1] + exponent_p_of_i[i]
+        factorial_p_exp_p = [0] * (n + 1)
+        for i in range(1, n+1):
+            factorial_p_exp_p[i] = factorial_p_exp_p[i-1] + exponent_p_of_i[i]
 
-    factorial_exp[p] = factorial_p_exp_p
+        factorial_exp[p] = factorial_p_exp_p
 
-unnecessary = []
-for k in range(0, N):
-    divisible = True
-    for p, a in factors.items():
-        f = factorial_exp[p]
-        exp_ck = f[n] - f[k] - f[n-k]
-        if exp_ck < a:
-            divisible = False
-            break
+    unnecessary = []
+    for k in range(0, N):
+        divisible = True
+        for p, a in factors.items():
+            f = factorial_exp[p]
+            exp_ck = f[n] - f[k] - f[n-k]
+            if exp_ck < a:
+                divisible = False
+                break
 
-    if divisible:
-        unnecessary.append(k + 1)
+        if divisible:
+            unnecessary.append(k + 1)
 
-print(len(unnecessary))
-if unnecessary:
-    print(' '.join(map(str, unnecessary)))
+    print(len(unnecessary))
+    if unnecessary:
+        print(' '.join(map(str, unnecessary)))
+        
+main()
