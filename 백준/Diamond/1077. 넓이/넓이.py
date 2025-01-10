bed9b3a32c52c337426494f552aef6aaf74c020d
김륜교 <165171093+kimryungyo@users.noreply.github.com>
2025-01-11 02:51:15 +0900

    1077 넓이 add comments.py

--- 백준/Diamond/1077. 넓이/넓이.py
+++ 백준/Diamond/1077. 넓이/넓이.py
@@ -3,14 +3,23 @@ import sys
 input = sys.stdin.readline
 
 def get_angle(point_1, point_2):
+    """
+    두 점 사이의 각도를 계산하는 함수
+    """
     return math.atan2(point_2[1] - point_1[1], point_2[0] - point_1[0])
 
 def sort_points_ccw(points):
+    """
+    점들을 반시계 방향으로 정렬하는 함수
+    """
     base_point = min(points, key=lambda point: (point[1], point[0]))
     sorted_points = sorted(points, key=lambda point: (get_angle(base_point, point), point[1], point[0]))
     return sorted_points
 
 def get_ccw(p1, p2, p3):
+    """
+    세 점의 방향성을 계산하는 함수 (Counter Clock Wise)
+    """
     x1, y1 = p1
     x2, y2 = p2
     x3, y3 = p3
@@ -19,6 +28,9 @@ def get_ccw(p1, p2, p3):
     return ccw
 
 def polygon_area(points):
+    """
+    다각형의 면적을 계산하는 함수 (Shoelace formula)
+    """
     n = len(points)
     coors = points.copy()
     coors.append(coors[0])
@@ -30,10 +42,16 @@ def polygon_area(points):
     return area
 
 def get_ccw_sign(p1, p2, p3):
+    """
+    세 점의 CCW 방향성을 부호로 반환하는 함수
+    """
     ccw_sign = math.copysign(1, get_ccw(p1, p2, p3))
     return ccw_sign
 
 def is_intersect(line_1, line_2):
+    """
+    두 선분이 교차하는지 여부를 판단하는 함수
+    """
     o1 = get_ccw_sign(*line_1, line_2[0])
     o2 = get_ccw_sign(*line_1, line_2[1])
     o3 = get_ccw_sign(*line_2, line_1[0])
@@ -43,12 +61,18 @@ def is_intersect(line_1, line_2):
     return False
 
 def find_intersect(line_1, lines):
+    """
+    주어진 선분과 교차하는 선분을 찾는 함수
+    """
     for line in lines:
         if is_intersect(line_1, line):
             return line
     return None
 
 def is_point_in_convex(point, convex):
+    """
+    점이 컨벡스헐 내부에 있는지 확인하는 함수
+    """
     sign = None
 
     convex_1 = convex[0]
@@ -65,6 +89,9 @@ def is_point_in_convex(point, convex):
     return True
 
 def get_points_in_convex(points, convex):
+    """
+    컨벡스헐 내의 점들과 교차하는 선분을 찾는 함수
+    """
     point_count = len(points)
 
     is_in_points = [ False ] * point_count
@@ -86,6 +113,9 @@ def get_points_in_convex(points, convex):
     return in_points, cross_lines
 
 def get_cross_point(line_1, line_2):
+    """
+    두 선분의 교차점을 계산하는 함수
+    """
     x1, y1 = line_1[0]
     x2, y2 = line_1[1]
     x3, y3 = line_2[0]
@@ -107,6 +137,9 @@ def get_cross_point(line_1, line_2):
     return (intersect_x, intersect_y)
 
 def get_cross_point_in_convex(line, convex):
+    """
+    선분과 컨벡스헐의 교차점을 찾는 함수
+    """
     convex_lines = [ (convex[i], convex[i+1]) for i in range(len(convex) - 1) ]
     convex_lines.append((convex[-1], convex[0]))
                     
@@ -116,6 +149,9 @@ def get_cross_point_in_convex(line, convex):
     return cross_point
 
 def find_cross_points(convex_1, convex_2):
+    """
+    두 컨벡스헐 사이의 모든 교차점을 찾는 함수
+    """
     convex_1_lines = [ (convex_1[i], convex_1[i+1]) for i in range(len(convex_1) - 1) ]
     convex_1_lines.append((convex_1[-1], convex_1[0]))
     
@@ -131,23 +167,68 @@ def find_cross_points(convex_1, convex_2):
 
     return cross_points
 
+# 입력 받기
 N, M = map(int, input().split())
 convex_1 = [ tuple(map(int, input().split())) for _ in range(N) ]
 convex_2 = [ tuple(map(int, input().split())) for _ in range(M) ]
 
+# 컨벡스헐 내부의 점들과 교차 선분 찾기
 in_points_1, cross_lines_1 = get_points_in_convex(convex_1, convex_2)
 cross_points_1 = [ get_cross_point_in_convex(line, convex_2) for line in cross_lines_1 ]
 
 in_points_2, cross_lines_2 = get_points_in_convex(convex_2, convex_1)
 cross_points_2 = [ get_cross_point_in_convex(line, convex_1) for line in cross_lines_2 ]
 
+# 두 컨벡스헐 간의 교차점 찾기
 cross_points = find_cross_points(convex_1, convex_2)
 
+# 교차점과 내부 점들을 합쳐 새로운 컨벡스헐을 생성
 convex_points = in_points_1 + cross_points_1 + in_points_2 + cross_points_2 + cross_points
 convex_points = set(convex_points)
-if len(convex_points) < 3: print(0); quit()
+if len(convex_points) < 3: 
+    print(0)
+    quit()
 
+# 점들을 반시계 방향으로 정렬
 sorted_convex_points = sort_points_ccw(convex_points)
 
+# 컨벡스헐의 면적 계산
 convex_area = polygon_area(sorted_convex_points)
 print(f"{convex_area:.10f}")
+
+# 디버깅 
+if __name__ == "__debugging__":
+
+    # 교차 컨벡스헐의 점들을 출력
+    print(sorted_convex_points)
+
+    def visualization(convex_1, convex_2, inter_convex):
+        """
+        컨벡스헐과 교차 컨벡스헐을 시각화하는 함수
+        matplotlib 라이브러리 설치 필요
+        """
+        import matplotlib.pyplot as plt
+        from matplotlib.patches import Polygon
+
+        polygon1 = convex_1
+        polygon2 = convex_2
+        polygon3 = inter_convex
+
+        fig, ax = plt.subplots()
+
+        poly1 = Polygon(polygon1, edgecolor='red', facecolor='red', alpha=0.5)
+        ax.add_patch(poly1)
+
+        poly2 = Polygon(polygon2, edgecolor='blue', facecolor='blue', alpha=0.5)
+        ax.add_patch(poly2)
+
+        poly3 = Polygon(polygon3, edgecolor='green', facecolor='green', alpha=0.5)
+        ax.add_patch(poly3)
+
+        plt.xlim(-10, 10)
+        plt.ylim(-10, 10)
+
+        plt.show()
+
+    # 두 컨벡스헐과 교차 컨벡스헐을 시각화
+    visualization(convex_1, convex_2, sorted_convex_points)
