a3b4e735bda12e00f3e33b69d39b27952a944244
김륜교 <165171093+kimryungyo@users.noreply.github.com>
2025-01-16 14:27:45 +0700

    [Platinum IV] Title: 물 채우기, Time: 320 ms, Memory: 39108 KB -BaekjoonHub

---
+++ 백준/Platinum/2276. 물 채우기/물 채우기.py
@@ -0,0 +1,32 @@
+from heapq import heappop, heappush
+from sys import stdin
+input = stdin.readline
+
+M, N = map(int, input().split())
+grid = [ list(map(int, input().split())) for _ in range(N) ]
+visited = [ [False] * M for _ in range(N) ]
+
+water = 10 ** 10
+queue = []
+for y in range(N):
+    for x in range(M):
+        if y == 0 or y == N-1 or x == 0 or x == M-1:
+            h = grid[y][x]
+            heappush(queue, (h, y, x))
+            visited[y][x] = True
+            if h < water: water = h
+
+answer = 0
+while queue:
+    h, y, x = heappop(queue)
+
+    answer += max(0, water - h)
+    if h > water: water = h
+
+    for dy, dx in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        ny, nx = y + dy, x + dx
+        if 0 <= ny < N and 0 <= nx < M and not visited[ny][nx]:
+            visited[ny][nx] = True
+            heappush(queue, (grid[ny][nx], ny, nx))
+
+print(answer)
