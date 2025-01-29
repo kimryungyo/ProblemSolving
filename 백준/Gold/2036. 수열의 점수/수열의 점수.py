dd53b782a2740f4e240a70f68b801b729286133c
김륜교 <165171093+kimryungyo@users.noreply.github.com>
2025-01-30 00:25:12 +0700

    [Gold IV] Title: 수열의 점수, Time: 144 ms, Memory: 122364 KB -BaekjoonHub

---
+++ 백준/Gold/2036. 수열의 점수/수열의 점수.py
@@ -0,0 +1,43 @@
+N, *nums = map(int, open(0))
+
+plus = []
+ones = 0
+zeros = 0
+minus = []
+
+for num in nums:
+    if num > 1:
+        plus.append(num)
+    elif num == 1:
+        ones += 1
+    elif num == 0:
+        zeros += 1
+    else:
+        minus.append(num)
+
+plus.sort(reverse=True)
+minus.sort()
+score = 0
+
+i = 0
+while i < len(plus):
+    if i + 1 < len(plus):
+        score += plus[i] * plus[i+1]
+        i += 2
+    else:
+        score += plus[i]
+        i += 1
+
+score += ones
+
+i = 0
+while i < len(minus):
+    if i + 1 < len(minus):
+        score += minus[i] * minus[i+1]
+        i += 2
+    else:
+        if zeros == 0:
+            score += minus[i]
+        i += 1
+
+print(score)
