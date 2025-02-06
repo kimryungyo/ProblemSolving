6be9d4849d6a590f47942e15d9d92b8a66bc8f87
김륜교 <165171093+kimryungyo@users.noreply.github.com>
2025-02-07 00:14:41 +0700

    [Gold I] Title: 끝말잇기, Time: 112 ms, Memory: 40128 KB -BaekjoonHub

--- 백준/Gold/2320. 끝말잇기/끝말잇기.py
+++ 백준/Gold/2320. 끝말잇기/끝말잇기.py
@@ -5,38 +5,46 @@ def convert_to_index(char):
     if char == "O": return 3
     if char == "U": return 4
 
-N = int(input())
-WORDS = []
-for idx in range(N):
-    word = input()
-    w_start = convert_to_index(word[0])
-    w_end = convert_to_index(word[-1])
-    info = (w_start, w_end, len(word), idx)
-    WORDS.append(info)
+def input_datas():
+    N = int(input())
+    WORDS = []
+    for idx in range(N):
+        word = input()
+        w_start = convert_to_index(word[0])
+        w_end = convert_to_index(word[-1])
+        info = (w_start, w_end, len(word), idx)
+        WORDS.append(info)
+    return N, WORDS
 
-dp = [ [ 0, 0, 0, 0, 0 ] for _ in range(65536) ]
-queue = []
+def main():
+    N, WORDS = input_datas()
+    created = [ [ False ] * 5 for _ in range(65536) ]
 
-initial = int('1' * N, 2)
-for start, end, leng, idx in WORDS:
-    used = initial & ~(1 << idx)
-    queue.append( (used, leng, end) )
+    queue = []
+    initial = int('1' * N, 2)
+    for start, end, leng, idx in WORDS:
+        used = initial & ~(1 << idx)
+        queue.append( (used, leng, end) )
+        created[used][end] = True
+    del initial
 
-max_len = 0
-while queue:
-    used, leng, end = queue.pop()
+    max_len = 0
+    while queue:
+        used, leng, end = queue.pop()
+        if leng > max_len:
+            max_len = leng
 
-    for i in range(N):
-        if used & (1 << i):
-            w_start, w_end, w_len, w_idx = WORDS[i]
+        for i in range(N):
+            if used & (1 << i):
+                w_start, w_end, w_len, w_idx = WORDS[i]
 
-            new_used = used & ~(1 << w_idx)
-            new_len = leng + w_len
+                new_used = used & ~(1 << w_idx)
+                new_len = leng + w_len
 
-            if w_start == end and dp[new_used][w_end] == 0:
-                dp[new_used][w_end] = new_len
-                queue.append( (new_used, new_len, w_end) )
-                if new_len > max_len:
-                    max_len = new_len
+                if w_start == end and not created[new_used][w_end]:
+                    created[new_used][w_end] = True
+                    queue.append( (new_used, new_len, w_end) )
 
-print(max_len)
+    return max_len
+
+print(main())
