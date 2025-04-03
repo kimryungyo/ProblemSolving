3eac0305b9e03eb95faf6ccfa25702fd12de4778
김륜교 <165171093+kimryungyo@users.noreply.github.com>
2025-04-04 05:13:13 +0900

    [Gold II] Title: 배드민턴 복식 팀 만들기, Time: 276 ms, Memory: 61920 KB -BaekjoonHub

--- 백준/Gold/23762. 배드민턴 복식 팀 만들기/배드민턴 복식 팀 만들기.py
+++ 백준/Gold/23762. 배드민턴 복식 팀 만들기/배드민턴 복식 팀 만들기.py
@@ -1,11 +1,5 @@
-# 우선 인접한 선수들끼리 실력차가 최소가 되도록 선수들을 실력 순으로 정렬한다.
-# 정렬된 순서에서 연속 4명을 팀으로 구성할 때의 실력차(최대-최소)를 비용으로 보고,
-# dp[i]를 i번째 선수부터 구성 가능한 (팀 수, 총 비용)으로 정의하여 DP를 진행한다.
-# team 선택 시 연속된 4명을 팀으로 구성하고, skip 선택 시 해당 선수를 경기에서 제외한다.
-# DP 복원 후, 팀에 포함되지 않은 선수의 원래 번호를 오름차순으로 출력한다.
-
-n, *rest = map(int, open(0).read().split())
-players = [(rest[i], i) for i in range(n)]
+n, *datas = map(int, open(0).read().split())
+players = [(datas[i], i) for i in range(n)]
 players.sort(key=lambda x: x[0])
 
 dp = [(0, 0)] * (n + 1)         # dp[i] : (팀 수, 총 비용)
@@ -39,5 +33,4 @@ skipped = [players[i][1] for i in range(n) if not used[i]]
 skipped.sort()
 
 print(total_cost)
-for s in skipped:
-    print(s)
+print("\n".join(map(str, skipped)))
