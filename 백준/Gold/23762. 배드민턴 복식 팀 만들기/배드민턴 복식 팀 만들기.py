b95af110ea22be3ba31e3f751ab189ff5e7b04c2
김륜교 <165171093+kimryungyo@users.noreply.github.com>
2025-04-04 04:48:30 +0900

    [Gold II] Title: 배드민턴 복식 팀 만들기, Time: 216 ms, Memory: 131600 KB -BaekjoonHub

---
+++ 백준/Gold/23762. 배드민턴 복식 팀 만들기/배드민턴 복식 팀 만들기.py
@@ -0,0 +1,40 @@
+n, *rest = map(int, open(0).read().split())
+players = [(rest[i], i) for i in range(n)]
+players.sort(key=lambda x: x[0])
+
+dp = [(0, 0)] * (n + 1)
+decision = [None] * (n + 1)
+dp[n] = (0, 0)
+
+for i in range(n - 1, -1, -1):
+    skip = dp[i + 1]
+    best = skip
+    best_decision = 'skip'
+    
+    if i + 3 < n:
+        candidate = (1 + dp[i + 4][0], (players[i + 3][0] - players[i][0]) + dp[i + 4][1])
+        if (candidate[0], -candidate[1]) > (best[0], -best[1]):
+            best = candidate
+            best_decision = 'team'
+    
+    dp[i] = best
+    decision[i] = best_decision
+
+total_cost = dp[0][1]
+
+used = [False] * n
+i = 0
+
+while i < n:
+    if decision[i] == 'team' and i + 3 < n:
+        used[i] = used[i + 1] = used[i + 2] = used[i + 3] = True
+        i += 4
+    else:
+        i += 1
+
+skipped = [players[i][1] for i in range(n) if not used[i]]
+skipped.sort()
+
+print(total_cost)
+for s in skipped:
+    print(s)
