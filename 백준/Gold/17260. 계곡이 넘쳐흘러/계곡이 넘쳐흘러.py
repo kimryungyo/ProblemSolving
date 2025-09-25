a291244e89f4508d8c8e53bba3be0108a62820ef
김륜교 <165171093+kimryungyo@users.noreply.github.com>
2025-09-26 01:30:32 +0900

    [Gold II] Title: 계곡이 넘쳐흘러, Time: 376 ms, Memory: 551412 KB -BaekjoonHub

---
+++ 백준/Gold/17260. 계곡이 넘쳐흘러/계곡이 넘쳐흘러.py
@@ -0,0 +1,29 @@
+from sys import stdin, setrecursionlimit
+input = stdin.readline
+setrecursionlimit(250000)
+
+N, K = map(int, input().split())
+heights = list(map(int, input().split()))
+graph = { i: [] for i in range(N) }
+
+for _ in range(N - 1):
+    a, b = map(int, input().split())
+    graph[a - 1].append(b - 1)
+    graph[b - 1].append(a - 1)
+
+def dfs(now, before, min_height):
+    for next in graph[now]:
+        if next == before: continue
+
+        if now != K -1 and heights[next] >= min_height:
+            print(1)
+            quit()
+
+        next_height = heights[next]
+        next_min_height = (min_height - next_height) * 2 + next_height
+
+        if next_min_height > 1000000: continue
+        dfs(next, now, next_min_height)
+
+dfs(K - 1, None, heights[K - 1])
+print(0)
